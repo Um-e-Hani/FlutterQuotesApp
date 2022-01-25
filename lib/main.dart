7423213c8b8550f546801be827e70eaf254d55f4
FlutLab.io <>
2022-01-25 21:06:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _QuoteListState extends State<QuoteList> {
       body: Column(
         children: quotes
             .map((quote) => Container(
-                margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
                 child: Column(
                   children: <Widget>[
                     Container(
